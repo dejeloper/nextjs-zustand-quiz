37cfcecc19bee613b9729dd0f068c9920807bd1c
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-10-31 11:03:58 -0500

    Add Reset

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,15 +1,22 @@
 import { useQuestionsResults } from "../hooks/useQuestionResults";
+import { useQuestionsStore } from "../store/questions";
 
 export const Footer = () => {
   const { correctAnswers, incorrectAnswers, unansweredQuestions } =
     useQuestionsResults();
-
+  const reset = useQuestionsStore((state) => state.reset);
   return (
     <footer className="rounded-lg shadow m-4 bg-gray-800">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+      <div className="flex flex-col w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
         <span className="text-sm sm:text-center text-gray-400">
           {`✅ ${correctAnswers} Correctas - ❌ ${incorrectAnswers} Incorrectas - ❔${unansweredQuestions} Sin responder`}
         </span>
+        <button
+          className="border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm mt-4 px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700"
+          onClick={() => reset()}
+        >
+          Reiniciar el Juego
+        </button>
       </div>
     </footer>
   );
--- src/app/store/questions.ts
+++ src/app/store/questions.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { type Question } from "../types/type";
 import confetti from "canvas-confetti";
+import { persist } from "zustand/middleware";
 
 interface State {
   questions: Question[];
@@ -9,63 +10,70 @@ interface State {
   selectAnswer: (questionId: number, answerIndex: number) => void;
   goNextQuestion: () => void;
   goPrevQuestion: () => void;
+  reset: () => void;
 }
 
-export const useQuestionsStore = create<State>((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0,
+export const useQuestionsStore = create<State>()(
+  persist(
+    (set, get) => {
+      return {
+        questions: [],
+        currentQuestion: 0,
 
-    fecthQuestions: async (limit: number) => {
-      const res = await fetch("http://localhost:3000/data/questions.json");
-      const json = await res.json();
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
+        fecthQuestions: async (limit: number) => {
+          const res = await fetch("http://localhost:3000/data/questions.json");
+          const json = await res.json();
+          const questions = json
+            .sort(() => Math.random() - 0.5)
+            .slice(0, limit);
 
-      // set((state) => {
-      //   return {
-      //     questions: [
-      //       {
-      //         id: 1,
-      //         question: "¿Quién ganó la Copa del Mundo de 2014?",
-      //         answers: ["Alemania", "Argentina", "Francia", "Brasil"],
-      //         correctAnswer: 0,
-      //       },
-      //     ],
-      //   };
-      // });
+          set({ questions });
+        },
 
-      set({ questions });
-    },
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          const { questions } = get();
+          const newQuestions = structuredClone(questions);
+          const questionIndex = newQuestions.findIndex(
+            (q) => q.id === questionId
+          );
+          const questionInfo = newQuestions[questionIndex];
+          const isCorrect = questionInfo.correctAnswer === answerIndex;
 
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get();
-      const newQuestions = structuredClone(questions);
-      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
-      const questionInfo = newQuestions[questionIndex];
-      const isCorrect = questionInfo.correctAnswer === answerIndex;
+          if (isCorrect) confetti();
 
-      if (isCorrect) confetti();
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrect,
+            userSelectedAnswer: answerIndex,
+          };
 
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrect,
-        userSelectedAnswer: answerIndex,
+          set({ questions: newQuestions });
+        },
+
+        goNextQuestion: () => {
+          const { questions, currentQuestion } = get();
+          const nextQuestion = currentQuestion + 1;
+          if (nextQuestion < questions.length)
+            set({ currentQuestion: nextQuestion });
+        },
+
+        goPrevQuestion: () => {
+          const { currentQuestion } = get();
+          const prevQuestion = currentQuestion - 1;
+          if (prevQuestion >= 0) set({ currentQuestion: prevQuestion });
+        },
+
+        reset: () => {
+          set({
+            questions: [],
+            currentQuestion: 0,
+          });
+        },
       };
-
-      set({ questions: newQuestions });
     },
-
-    goNextQuestion: () => {
-      const { questions, currentQuestion } = get();
-      const nextQuestion = currentQuestion + 1;
-      if (nextQuestion < questions.length)
-        set({ currentQuestion: nextQuestion });
-    },
-
-    goPrevQuestion: () => {
-      const { currentQuestion } = get();
-      const prevQuestion = currentQuestion - 1;
-      if (prevQuestion >= 0) set({ currentQuestion: prevQuestion });
-    },
-  };
-});
+    {
+      name: "questions",
+      getStorage: () => localStorage,
+    }
+  )
+);
