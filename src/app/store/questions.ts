de6bbee72b5ffb6c3c1188997ec3407357318134
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-10-31 11:19:27 -0500

    Add middleware devtools

--- src/app/store/questions.ts
+++ src/app/store/questions.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { type Question } from "../types/type";
 import confetti from "canvas-confetti";
-import { persist } from "zustand/middleware";
+import { persist, devtools } from "zustand/middleware";
 import { getAllQuestions } from "../services/questions";
 
 interface State {
@@ -15,65 +15,72 @@ interface State {
 }
 
 export const useQuestionsStore = create<State>()(
-  persist(
-    (set, get) => {
-      return {
-        questions: [],
-        currentQuestion: 0,
+  devtools(
+    persist(
+      (set, get) => {
+        return {
+          questions: [],
+          currentQuestion: 0,
 
-        fecthQuestions: async (limit: number) => {
-          const json = await getAllQuestions();
-          const questions = json
-            .sort(() => Math.random() - 0.5)
-            .slice(0, limit);
+          fecthQuestions: async (limit: number) => {
+            const json = await getAllQuestions();
+            const questions = json
+              .sort(() => Math.random() - 0.5)
+              .slice(0, limit);
 
-          set({ questions });
-        },
+            set({ questions }, false, "Fetch Questions");
+          },
 
-        selectAnswer: (questionId: number, answerIndex: number) => {
-          const { questions } = get();
-          const newQuestions = structuredClone(questions);
-          const questionIndex = newQuestions.findIndex(
-            (q) => q.id === questionId
-          );
-          const questionInfo = newQuestions[questionIndex];
-          const isCorrect = questionInfo.correctAnswer === answerIndex;
+          selectAnswer: (questionId: number, answerIndex: number) => {
+            const { questions } = get();
+            const newQuestions = structuredClone(questions);
+            const questionIndex = newQuestions.findIndex(
+              (q) => q.id === questionId
+            );
+            const questionInfo = newQuestions[questionIndex];
+            const isCorrect = questionInfo.correctAnswer === answerIndex;
 
-          if (isCorrect) confetti();
+            if (isCorrect) confetti();
 
-          newQuestions[questionIndex] = {
-            ...questionInfo,
-            isCorrect,
-            userSelectedAnswer: answerIndex,
-          };
+            newQuestions[questionIndex] = {
+              ...questionInfo,
+              isCorrect,
+              userSelectedAnswer: answerIndex,
+            };
 
-          set({ questions: newQuestions });
-        },
+            set({ questions: newQuestions }, false, "Select Answer");
+          },
 
-        goNextQuestion: () => {
-          const { questions, currentQuestion } = get();
-          const nextQuestion = currentQuestion + 1;
-          if (nextQuestion < questions.length)
-            set({ currentQuestion: nextQuestion });
-        },
+          goNextQuestion: () => {
+            const { questions, currentQuestion } = get();
+            const nextQuestion = currentQuestion + 1;
+            if (nextQuestion < questions.length)
+              set({ currentQuestion: nextQuestion }, false, "Next Question");
+          },
 
-        goPrevQuestion: () => {
-          const { currentQuestion } = get();
-          const prevQuestion = currentQuestion - 1;
-          if (prevQuestion >= 0) set({ currentQuestion: prevQuestion });
-        },
+          goPrevQuestion: () => {
+            const { currentQuestion } = get();
+            const prevQuestion = currentQuestion - 1;
+            if (prevQuestion >= 0)
+              set({ currentQuestion: prevQuestion }, false, "Prev Question");
+          },
 
-        reset: () => {
-          set({
-            questions: [],
-            currentQuestion: 0,
-          });
-        },
-      };
-    },
-    {
-      name: "questions",
-      getStorage: () => localStorage,
-    }
+          reset: () => {
+            set(
+              {
+                questions: [],
+                currentQuestion: 0,
+              },
+              false,
+              "Reset"
+            );
+          },
+        };
+      },
+      {
+        name: "questions",
+        getStorage: () => localStorage,
+      }
+    )
   )
 );
